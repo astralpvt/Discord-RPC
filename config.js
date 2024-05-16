fb55d6fd6e3f937b43da2e43a6eb388afbb463b4
Yutae11 <yutaokkotsu1110@gmail.com>
2024-05-16 14:43:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
   FirstButtonUrl: "https://www.instagram.com/yuta.xae?igsh=MTVzenNodGt3MmE5cw==",
   SecondButtonName: "DISCORD",
   SecondButtonUrl: "https://discord.com/invite/sbFJCnQzJz",
-  LargeImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240186512079851592/071a36c7a48767b3a56465d712525a7a.gif?ex=6645a532&is=664453b2&hm=beb1a0521ef563c99b76fc923c6958114f141508f3fa41fedb8e3f61c29341c6&", // DISCORD CDN IMAGE ONLY
+  LargeImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240592001607536681/371c7e7ebdf81a43ee3c3de118609231.gif?ex=66471ed6&is=6645cd56&hm=12015b1c45450b80aa2c103a829e19eb29c35861f98b306a75a8f9484d7f439c&", // DISCORD CDN IMAGE ONLY
   LargeText: "discord.js", // hover text for large image
-  SmallImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240186512079851592/071a36c7a48767b3a56465d712525a7a.gif?ex=6645a532&is=664453b2&hm=beb1a0521ef563c99b76fc923c6958114f141508f3fa41fedb8e3f61c29341c6&", // DISCORD CDN IMAGE ONLY
+  SmallImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240592031739543553/74e95fa7757a7c3c99c3e67675146208.gif?ex=66471edd&is=6645cd5d&hm=39b602b50b9d72b93fd9be01fcb828c9c9590e42abd93c5315c39513ccb62cfb&", // DISCORD CDN IMAGE ONLY
   SmallText: "creating servers", // hover text for small image
 };
 
