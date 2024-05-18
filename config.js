8da133c8b658a80b03a7ad3b6f22b35bbf2b514a
astralpvt <astralpvt@outlook.com>
2024-05-18 06:39:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ module.exports = {
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
   Name: "My bio",
-  State: "CREATING SERVERS",
+  State: "LEARNING",
   Details: "DISCORD DEVELOPER",
   FirstButtonName: "INSTA",
-  FirstButtonUrl: "https://www.instagram.com/yuta.xae?igsh=MTVzenNodGt3MmE5cw==",
+  FirstButtonUrl: "https://youtu.be/xvFZjo5PgG0?si=ioUymXKIzVjZfjVe",
   SecondButtonName: "DISCORD",
-  SecondButtonUrl: "https://discord.com/invite/sbFJCnQzJz",
-  LargeImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240592001607536681/371c7e7ebdf81a43ee3c3de118609231.gif?ex=66471ed6&is=6645cd56&hm=12015b1c45450b80aa2c103a829e19eb29c35861f98b306a75a8f9484d7f439c&", // DISCORD CDN IMAGE ONLY
+  SecondButtonUrl: "https://discord.gg/calestials",
+  LargeImage: "https://cdn.discordapp.com/emojis/1230036812949360661.gif?v=1&size=48&quality=lossless ", // DISCORD CDN IMAGE ONLY
   LargeText: "discord.js", // hover text for large image
-  SmallImage: "https://cdn.discordapp.com/attachments/1219627413831553034/1240592031739543553/74e95fa7757a7c3c99c3e67675146208.gif?ex=66471edd&is=6645cd5d&hm=39b602b50b9d72b93fd9be01fcb828c9c9590e42abd93c5315c39513ccb62cfb&", // DISCORD CDN IMAGE ONLY
+  SmallImage: "https://cdn.discordapp.com/emojis/1071105387241152622.gif?v=1&size=48&quality=lossless ", // DISCORD CDN IMAGE ONLY
   SmallText: "creating servers", // hover text for small image
 };
 
