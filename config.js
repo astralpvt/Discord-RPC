a6788164cf6e18da90b85d23f23ab53192e4b239
astralpvt <astralpvt@outlook.com>
2024-05-25 08:30:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
   FirstButtonUrl: "https://youtu.be/xvFZjo5PgG0?si=ioUymXKIzVjZfjVe",
   SecondButtonName: "DISCORD",
   SecondButtonUrl: "https://discord.gg/calestials",
-  LargeImage: "https://cdn.discordapp.com/emojis/1230036812949360661.gif?v=1&size=48&quality=lossless ", // DISCORD CDN IMAGE ONLY
+  LargeImage: "https://cdn.discordapp.com/attachments/1243220472569794648/1243760144601321542/SPOILER_lv_0_20240524073828.gif?ex=6652a566&is=665153e6&hm=22faf9a6277899b572f6954fff598d8a330d2d688d6cfe6bb6e0e1418108f315& ", // DISCORD CDN IMAGE ONLY
   LargeText: "discord.js", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/emojis/1071105387241152622.gif?v=1&size=48&quality=lossless ", // DISCORD CDN IMAGE ONLY
   SmallText: "creating servers", // hover text for small image
